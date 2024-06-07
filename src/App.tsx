272b6196e205016f464c459a1341bef4fb3b55b7
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-06-06 21:12:13 -0400

    fixing some prettyful and console.logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,45 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import getCorgi from './services/api'
-import emailjs from '@emailjs/browser';
+import { useEffect, useState } from "react";
+import "./App.css";
+import getCorgi from "./services/api";
+import emailjs from "@emailjs/browser";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [corgi, setCorgi] = useState('')
+  const [count, setCount] = useState(0);
+  const [corgi, setCorgi] = useState("");
   useEffect(() => {
     getCorgi().then((data) => {
-      setCorgi(data.message)
-    })
+      setCorgi(data.message);
+    });
     emailjs.init({
-      publicKey: 'LYXAyujnU3l-YVifg'
-    })
-  }, [count, setCorgi])
+      publicKey: "LYXAyujnU3l-YVifg",
+    });
+  }, [count, setCorgi]);
 
   const handleClicking = async () => {
     getCorgi()
-    .then((data) => setCorgi(data.message))
-    .then(() => setCount(count+1))
-    if(count === 2) {
-      emailjs
-      .send('service_b4xdu89', 'template_ot9nrsx')
-      .then(
+      .then((data) => setCorgi(data.message))
+      .then(() => setCount(count + 1));
+    if (count === 2) {
+      emailjs.send("service_b4xdu89", "template_ot9nrsx").then(
         () => {
-          console.log('SUCCESS!');
+          console.log("SUCCESS!");
         },
         (error) => {
-          console.log('FAILED...', error);
-        },
+          console.log("FAILED...", error);
+        }
       );
     }
-  }
+  };
   return (
     <>
-    <div className='main'>
-      <h1>Corgi Clicker</h1>
-      <p>You've found {`${count}`} new pictures, I hope you're happy</p>
-      <button onClick={()=> handleClicking()}>
-        Get New Corgi
-      </button>
-      <img className='picture' src={corgi} alt="Corgi" />
-    </div>
+      <div className="main">
+        <h1>Corgi Clicker</h1>
+        <p>You've found {`${count}`} new pictures, I hope you're happy</p>
+        <button onClick={() => handleClicking()}>Get New Corgi</button>
+        <img className="picture" src={corgi} alt="Corgi" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,11 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: `https://dog.ceo/api/`,
 });
 
-export default async function getCorgi(){
+export default async function getCorgi() {
   const { data } = await api.get(`breed/pembroke/images/random`);
-  console.log(data);
   return data;
 }
